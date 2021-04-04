0af45bbf5edacfccdc2d985854918d9dbc779893
Molnár Tímea <molnart@sonrisa.hu>
2021-04-04 20:42:56 +0200

    Add permission asking at app starting

--- app/src/main/java/com/learntodroid/simplealarmclock/activities/MainActivity.java
+++ app/src/main/java/com/learntodroid/simplealarmclock/activities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.learntodroid.simplealarmclock.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
@@ -15,6 +19,8 @@ import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+
     @BindView(R.id.bottom_navigation) BottomNavigationView bottomNavigationView;
 
     @Override
@@ -25,10 +31,34 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         setupBottomNavigation();
+        askPermissionIfMissing();
     }
 
     private void setupBottomNavigation() {
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.activity_main_nav_host_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navHostFragment.getNavController());
+    }
+
+    private void askPermissionIfMissing() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
+            /* TODO user friendly explanation
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.SEND_SMS)) { */
+            ActivityCompat.requestPermissions(this,
+                    new String[]{ Manifest.permission.SEND_SMS },
+                    MY_PERMISSIONS_REQUEST_SEND_SMS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_SEND_SMS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // TODO granted
+                } else {
+                    // TODO denied
+                }
+            }
+        }
     }
 }
